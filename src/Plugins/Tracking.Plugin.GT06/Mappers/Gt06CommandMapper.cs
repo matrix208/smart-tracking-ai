3dff879d7ae17baeff2b7e8f00b21e8928c6f2e7
Telal <reco200814@gmail.com>
2026-08-03 13:40:36 +0300

    Before command model migration

--- src/Plugins/Tracking.Plugin.GT06/Mappers/Gt06CommandMapper.cs
+++ src/Plugins/Tracking.Plugin.GT06/Mappers/Gt06CommandMapper.cs
@@ -9,44 +9,140 @@ internal static class Gt06CommandMapper
     {
         return command.Name.ToLowerInvariant() switch
         {
-            DeviceCommandNames.Position     => "DWXX#",
-            DeviceCommandNames.Status       => "STATUS#",
-            DeviceCommandNames.Reboot       => "RESET#",
+            DeviceCommandNames.Position   => "DWXX#",
+            DeviceCommandNames.Status     => "STATUS#",
+            DeviceCommandNames.Check      => "CHECK#",
+            DeviceCommandNames.Version    => "VERSION#",
+            DeviceCommandNames.Imei       => "IMEI#",
 
-            DeviceCommandNames.OilOn        => "RELAY,1#",
-            DeviceCommandNames.OilOff       => "RELAY,0#",
+            DeviceCommandNames.Reboot     => "RESET#",
+            DeviceCommandNames.Reset      => "RESET#",
+            DeviceCommandNames.Factory    => "FACTORY#",
+            DeviceCommandNames.RestartGps => "GPSRESET#",
 
-            DeviceCommandNames.RelayOn      => "RELAY,1#",
-            DeviceCommandNames.RelayOff     => "RELAY,0#",
+            DeviceCommandNames.OilOn      => "RELAY,1#",
+            DeviceCommandNames.OilOff     => "RELAY,0#",
+            DeviceCommandNames.RelayOn    => "RELAY,1#",
+            DeviceCommandNames.RelayOff   => "RELAY,0#",
 
-            DeviceCommandNames.Monitor      => "MONITOR#",
-            DeviceCommandNames.Tracker      => "TRACKER#",
+            DeviceCommandNames.Monitor    => "MONITOR#",
+            DeviceCommandNames.Tracker    => "TRACKER#",
 
-            DeviceCommandNames.Upload       => "UPLOAD#",
-            DeviceCommandNames.Check        => "CHECK#",
+            DeviceCommandNames.Upload     => BuildUpload(command),
+            DeviceCommandNames.Apn        => BuildApn(command),
+            DeviceCommandNames.Server     => BuildServer(command),
+            DeviceCommandNames.TimeZone   => BuildTimeZone(command),
+            DeviceCommandNames.Password   => BuildPassword(command),
 
-            DeviceCommandNames.Apn          => "APN#",
-            DeviceCommandNames.Server       => "SERVER#",
-            DeviceCommandNames.TimeZone     => "TIMEZONE#",
-            DeviceCommandNames.Password     => "PASSWORD#",
+            DeviceCommandNames.Sos        => BuildSos(command),
+            DeviceCommandNames.Shock      => BuildShock(command),
+            DeviceCommandNames.Move       => BuildMove(command),
+            DeviceCommandNames.Speed      => BuildSpeed(command),
+            DeviceCommandNames.Sleep      => BuildSleep(command),
+            DeviceCommandNames.Heartbeat  => BuildHeartbeat(command),
 
-            DeviceCommandNames.Factory      => "FACTORY#",
-            DeviceCommandNames.RestartGps   => "GPSRESET#",
-            DeviceCommandNames.Reset        => "RESET#",
-
-            DeviceCommandNames.Sos          => "SOS#",
-            DeviceCommandNames.Shock        => "SHOCK#",
-            DeviceCommandNames.Move         => "MOVE#",
-            DeviceCommandNames.Speed        => "SPEED#",
-            DeviceCommandNames.Sleep        => "SLEEP#",
-            DeviceCommandNames.Heartbeat    => "HEART#",
-
-            DeviceCommandNames.Imei         => "IMEI#",
-            DeviceCommandNames.Version      => "VERSION#",
-            DeviceCommandNames.Signal       => "CSQ#",
+            DeviceCommandNames.Signal     => "CSQ#",
 
             _ => throw new NotSupportedException(
                 $"GT06 command '{command.Name}' is not supported.")
         };
+    }
+
+    private static string BuildUpload(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("interval", out var value))
+            return "UPLOAD#";
+
+        return $"UPLOAD,{value}#";
+    }
+
+    private static string BuildApn(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("apn", out var apn))
+            return "APN#";
+
+        var result = $"APN,{apn}";
+
+        if (command.Parameters.TryGetValue("user", out var user))
+            result += $",{user}";
+
+        if (command.Parameters.TryGetValue("password", out var password))
+            result += $",{password}";
+
+        return result + "#";
+    }
+
+    private static string BuildServer(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("host", out var host))
+            return "SERVER#";
+
+        command.Parameters.TryGetValue("port", out var port);
+
+        return $"SERVER,1,{host},{port ?? 5001}#";
+    }
+
+    private static string BuildTimeZone(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("offset", out var offset))
+            return "TIMEZONE#";
+
+        return $"TIMEZONE,{offset}#";
+    }
+
+    private static string BuildPassword(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("password", out var password))
+            return "PASSWORD#";
+
+        return $"PASSWORD,{password}#";
+    }
+
+    private static string BuildSos(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("numbers", out var numbers))
+            return "SOS#";
+
+        return $"SOS,{numbers}#";
+    }
+
+    private static string BuildShock(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("level", out var level))
+            return "SHOCK#";
+
+        return $"SHOCK,{level}#";
+    }
+
+    private static string BuildMove(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("radius", out var radius))
+            return "MOVE#";
+
+        return $"MOVE,{radius}#";
+    }
+
+    private static string BuildSpeed(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("limit", out var limit))
+            return "SPEED#";
+
+        return $"SPEED,{limit}#";
+    }
+
+    private static string BuildSleep(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("mode", out var mode))
+            return "SLEEP#";
+
+        return $"SLEEP,{mode}#";
+    }
+
+    private static string BuildHeartbeat(DeviceCommand command)
+    {
+        if (!command.Parameters.TryGetValue("interval", out var interval))
+            return "HEART#";
+
+        return $"HEART,{interval}#";
     }
 }
